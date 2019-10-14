e61557e05b1f775d5ede73b8c74a5931222f3481
ga2arch <gabriele.carrettoni@gmail.com>
2019-10-14 20:33:01 +0200

    better input processing

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,14 +3,20 @@ import bowling.{BowlingGame, LineParser}
 object Main {
 
   def main(args: Array[String]): Unit = {
-    Iterator.continually(io.StdIn.readLine)
+    println("Bowling line score calculator")
+    Iterator.continually(prompt())
       .takeWhile(line => !line.isEmpty)
       .foreach(line => {
         runStandardGame(line) match {
-          case Right(game) => println(s"$line\tscore: ${game.score}")
+          case Right(game) => println(game.score)
           case Left(error) => println(s"$line\terror: $error")
         }
       })
+  }
+
+  private def prompt() = {
+    print("> ")
+    io.StdIn.readLine()
   }
 
   private def runStandardGame(line: String) = BowlingGame.run(BowlingGame.standard, LineParser.string)(line)
